4f61d361d3ffb508db59f6d43a6664729e23bdbf
hersson ceballos <herssonceballos@gmail.com>
2020-08-05 21:10:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,19 +1,9 @@
 def square_array(numbers)
   new_numbers = []
- numbers.each do |number|
+  numbers.each do |number|
     new_numbers.push(number ** 2 )
   end 
 end
 
 
-
-def square_array(numbers)
-  # your code here
-  squares = []
-  numbers.each do |number| 
-    puts number**2
-    squares.push(number ** 2)
-  end
-  return squares
-end
 
